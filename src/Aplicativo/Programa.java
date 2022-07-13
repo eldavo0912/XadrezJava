cf6e3909ad8ee487c10c1676b07e3db68c5f8411
Davi Amaral Martins <davimartins444@gmail.com>
2022-07-13 19:49:21 -0300

    Classe Posição criada

--- src/Aplicativo/Programa.java
+++ src/Aplicativo/Programa.java
@@ -1,10 +1,14 @@
 package Aplicativo;
 
+import Tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Posicao pos = new Posicao(3,5);
+		System.out.println(pos);
 	}
 
 }
