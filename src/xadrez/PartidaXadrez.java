04b8f233eba174df228e3ac7ec9928d727054a2d
Davi Amaral Martins <davimartins444@gmail.com>
2022-07-18 14:11:55 -0300

    adcionando Cores as peças

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -27,8 +27,13 @@ public class PartidaXadrez {
 	
 	private void iniciarPartida(){
 		colocarNovaPeca('e',1,new Rei(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('a',1,new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('h',1,new Torre(tabuleiro, Cor.BRANCO));
+		
+		
+		colocarNovaPeca('e',8,new Rei(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('a',8,new Torre(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('h',8,new Torre(tabuleiro, Cor.PRETO));
-
+		
 	}
 }
